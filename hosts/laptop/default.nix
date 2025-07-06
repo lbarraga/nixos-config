9c80eed2f8512a3696c377198d5289c322efbbe1
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-07-06 13:46:43 +0200

    feat[bluetooth]: enable bluetooth + icon tray

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -5,4 +5,8 @@
     ../../nixos/default.nix
     ./hardware-configuration.nix
   ];
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
 }
--- nixos/default.nix
+++ nixos/default.nix
@@ -5,6 +5,7 @@
     ./modules/bootloader.nix
     ./modules/sound.nix
     ./modules/system.nix
+    ./modules/bluetooth.nix
     ./modules/desktop/hyprland.nix
     ./modules/packages/tailscale.nix
   ];
