4e5d54f41104fd5aa3dc785a3c5b7eddabb7028f
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-07-13 00:37:10 +0200

    feat[hyprland + waybar]: static workspaces + azerty fix

--- home/packages/hypr/waybar/scripts/tailscale.sh
+++ home/packages/hypr/waybar/scripts/tailscale.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 if tailscale status 2>&1 | grep -q "Tailscale is stopped"; then
-    echo '{"text": " Tailscale", "class": "custom-tailscale", "alt": "tailscale_offline"}'
+    echo '{"text": "  ", "class": "custom-tailscale", "alt": "tailscale_offline"}'
 else
-    echo '{"text": " Tailscale", "class": "custom-tailscale", "alt": "tailscale_online"}'
+    echo '{"text": "  ", "class": "custom-tailscale", "alt": "tailscale_online"}'
 fi
