f4e7045e18f2f392861cfed85210652ed319ac8f
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-07-20 23:08:34 +0200

    feat[brave]: add extension (youtube sponsor block)

--- home/packages/brave-browser.nix
+++ home/packages/brave-browser.nix
@@ -9,6 +9,7 @@
       { id = "dhlnjfhjjbminbjbegeiijdakdkamjoi"; } # nord theme
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden
       { id = "hjfkenebldkfgibelglepinlabpjfbll"; } # no youtube shorts
+      { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # Youtube sponsor block
     ];
     commandLineArgs = [ "--disable-features=WebRtcAllowInputVolumeAdjustment" ];
   };
