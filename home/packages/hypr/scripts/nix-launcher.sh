e3b0e12226ff20c03aa6383ff850cc3e5f2120ba
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-12-23 23:19:02 +0100

    hypr[rofi]: allow unfree in launcher

--- home/packages/hypr/scripts/nix-launcher.sh
+++ home/packages/hypr/scripts/nix-launcher.sh
@@ -78,12 +78,14 @@ rm "$HISTORY_FILE.tmp"
 
 # --- Execution ---
 
+export NIXPKGS_ALLOW_UNFREE=1
+
 if [ "$EXIT_CODE" -eq 10 ]; then
   # Mode 1: Terminal (Shift+Enter)
   # Note: We quote $PKG inside the sh -c string to keep shellcheck happy and safe
-  kitty --title "Nix Run: $PKG" sh -c "echo 'Fetching $PKG...'; nix run nixpkgs#\"$PKG\"; echo '';"
+  kitty --title "Nix Run: $PKG" sh -c "echo 'Fetching $PKG...'; nix run --impure nixpkgs#\"$PKG\"; echo '';"
 else
   # Mode 2: Silent (Enter)
   notify-send "Nix Run" "Fetching and launching $PKG..."
-  nohup nix run nixpkgs#"$PKG" >/dev/null 2>&1 &
+  nohup nix run --impure nixpkgs#"$PKG" >/dev/null 2>&1 &
 fi
