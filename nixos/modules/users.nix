e749592ab481d3ebdc34174c0f3beb96698a5fba
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-02-15 21:32:14 +0100

    add plugdev group

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -8,12 +8,18 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = options.programs.nix-ld.libraries.default;
 
+  users.groups.plugdev = { };
+
   users.users.${settings.username} = {
     isNormalUser = true;
     description = settings.userFullName;
     extraGroups = [ "networkmanager" "wheel" "dialout" "plugdev" ];
     shell = pkgs.zsh;
   };
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000c", MODE="0660", GROUP="users", TAG+="uaccess"
+  '';
 
   security = {
     sudo.wheelNeedsPassword = true;
