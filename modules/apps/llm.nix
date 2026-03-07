079f5e6c27caa89b9be06a37a0de57c6997b3edb
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-07 22:19:28 +0100

    remove openwebui

--- modules/apps/llm.nix
+++ modules/apps/llm.nix
@@ -2,24 +2,18 @@
 
 {
   flake.modules.nixos.llm = {
-    # Enable the Open-WebUI service
-    services.open-webui = {
-      enable = true;
-      host = "127.0.0.1";
-      port = 8080;
-      # openFirewall = true; # Uncomment this if you want to access the UI from your laptop/phone on the local network
-    };
-
     home-manager.sharedModules = [ inputs.self.modules.homeManager.llm ];
   };
 
-  flake.modules.homeManager.llm = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      # llama.cpp with NVIDIA GPU acceleration enabled
-      (llama-cpp.override { cudaSupport = true; })
+  flake.modules.homeManager.llm =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        # llama.cpp with NVIDIA GPU acceleration enabled
+        (llama-cpp.override { cudaSupport = true; })
 
-      # Python tool that includes huggingface-cli for easy model downloading
-      python3Packages.huggingface-hub
-    ];
-  };
+        # Python tool that includes huggingface-cli for easy model downloading
+        python3Packages.huggingface-hub
+      ];
+    };
 }
