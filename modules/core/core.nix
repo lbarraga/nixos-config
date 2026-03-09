06ee97cd3e0b5f04c6d3a4dd4da2363cc94fed25
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-09 17:23:45 +0100

    add nix ld

--- modules/core/core.nix
+++ modules/core/core.nix
@@ -1,21 +1,31 @@
 {
-  flake.modules.nixos.core = { config, ... }: {
+  flake.modules.nixos.core =
+    { config, options, ... }:
+    {
 
-    # Bootloader
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+      # Bootloader
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
 
-    # Networking basics (hostname is defined in the specific host feature)
-    networking.networkmanager.enable = true;
-    services.resolved.enable = true;
+      # Networking basics (hostname is defined in the specific host feature)
+      networking.networkmanager.enable = true;
+      services.resolved.enable = true;
 
-    # Nix and System settings
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-    nixpkgs.config.allowUnfree = true;
-    system.stateVersion = "25.05";
+      # Nix and System settings
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      nixpkgs.config.allowUnfree = true;
+      system.stateVersion = "25.05";
 
-    # Time and Locale
-    time.timeZone = "Europe/Brussels";
-    i18n.defaultLocale = "nl_BE.UTF-8";
-  };
+      programs.nix-ld = {
+        enable = true;
+        libraries = options.programs.nix-ld.libraries.default;
+      };
+
+      # Time and Locale
+      time.timeZone = "Europe/Brussels";
+      i18n.defaultLocale = "nl_BE.UTF-8";
+    };
 }
