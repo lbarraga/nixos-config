a3879b01e3fde8226746922ade79e669a2e620a2
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-16 00:03:14 +0100

    lukas gone from embedded dev

--- modules/apps/embedded-dev.nix
+++ modules/apps/embedded-dev.nix
@@ -1,12 +1,16 @@
 {
   den.aspects.embedded-dev = {
-    nixos = {
-      users.groups.plugdev = { };
-      users.users.lukasbt.extraGroups = [ "plugdev" ];
+    nixos =
+      { user, ... }:
+      {
+        users.groups.plugdev = { };
+        services.udev.extraRules = ''
+          SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000c", MODE="0660", GROUP="users", TAG+="uaccess"
+        '';
+      };
 
-      services.udev.extraRules = ''
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000c", MODE="0660", GROUP="users", TAG+="uaccess"
-      '';
+    user = {
+      extraGroups = [ "plugdev" ];
     };
   };
 }
