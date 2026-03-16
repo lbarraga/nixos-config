66dd84e31eae1aab53231a6b5aaefa1d0e6a2606
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-16 16:56:15 +0100

    ready for server

--- modules/hosts/server/server.nix
+++ modules/hosts/server/server.nix
@@ -1,17 +1,28 @@
-{den, ...}: {
-  den.hosts.x86_64-linux.server.users.lukasbt = {};
+{ den, ... }:
+{
+  den.hosts.x86_64-linux.server.users.lukasbt = { };
 
   den.aspects.server = {
     includes = with den.aspects; [
       core
+
+      # hardware
       nvidia
       ignore-laptop-lid
+
+      # connectivity
       ssh-server
       tailscale
+
+      # cli
+      cli-tools
+      nh
+      neovim
+      zsh
     ];
 
     nixos = {
-      imports = [./_hardware-configuration.nix];
+      imports = [ ./_hardware-configuration.nix ];
       networking.hostName = "server";
     };
   };
