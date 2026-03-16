8393e51f1d6a97f6d47f9678c3109f1995150d9e
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-16 14:31:15 +0100

    add lukasbt back to embedded dev

--- modules/apps/embedded-dev.nix
+++ modules/apps/embedded-dev.nix
@@ -1,14 +1,14 @@
 {
   den.aspects.embedded-dev = {
-    nixos = {user, ...}: {
-      users.groups.plugdev = {};
-      services.udev.extraRules = ''
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000c", MODE="0660", GROUP="users", TAG+="uaccess"
-      '';
-    };
+    nixos =
+      { user, ... }:
+      {
+        users.groups.plugdev = { };
+        users.users.lukasbt.extraGroups = [ "plugdev" ];
 
-    user = {
-      extraGroups = ["plugdev"];
-    };
+        services.udev.extraRules = ''
+          SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="000c", MODE="0660", GROUP="users", TAG+="uaccess"
+        '';
+      };
   };
 }
