154edc0d9deb9019c75ec980701a9665eec1d397
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-17 15:29:10 +0100

    set gnome font

--- modules/desktop-environment/gnome.nix
+++ modules/desktop-environment/gnome.nix
@@ -8,5 +8,16 @@
       services.displayManager.gdm.enable = true;
       services.desktopManager.gnome.enable = true;
     };
+
+    homeManager = {
+      dconf.settings = {
+        "org/gnome/desktop/interface" = {
+          font-name = "JetBrainsMono Nerd Font 11";
+          document-font-name = "JetBrainsMono Nerd Font 11";
+          monospace-font-name = "JetBrainsMono Nerd Font 10";
+          titlebar-font = "JetBrainsMono Nerd Font Bold 11";
+        };
+      };
+    };
   };
 }
