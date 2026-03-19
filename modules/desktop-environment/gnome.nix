d8a32c830660897f948ea58a1978919465de0aac
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-19 12:36:42 +0100

    some gnome settings

--- modules/desktop-environment/gnome.nix
+++ modules/desktop-environment/gnome.nix
@@ -1,12 +1,23 @@
 {
   den.aspects.gnome = {
-    nixos = {
-      # Enable the X11 windowing system
+    nixos = {pkgs, ...}: {
       services.xserver.enable = true;
 
-      # Enable the GNOME Desktop Environment and GDM display manager
       services.displayManager.gdm.enable = true;
       services.desktopManager.gnome.enable = true;
+
+      # dont want this
+      environment.gnome.excludePackages = with pkgs; [
+        gnome-tour
+        epiphany # web browser
+        geary # email reader
+      ];
+
+      environment.systemPackages = with pkgs; [
+        gnome-tweaks
+        gnomeExtensions.dash-to-dock
+        gnomeExtensions.appindicator
+      ];
     };
 
     homeManager = {
