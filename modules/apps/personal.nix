1e80ea60fec5a3a29ebc0c85b7f439532a283a92
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-03-27 21:09:45 +0100

    autostart synology drive

--- modules/apps/personal.nix
+++ modules/apps/personal.nix
@@ -8,6 +8,20 @@
         synology-drive-client
         anki
       ];
+
+      systemd.user.services.synology-drive = {
+        Unit = {
+          Description = "Synology Drive Client Autostart";
+          After = ["graphical-session.target"];
+        };
+        Install = {
+          WantedBy = ["graphical-session.target"];
+        };
+        Service = {
+          ExecStart = "${pkgs.synology-drive-client}/bin/synology-drive";
+          Restart = "on-failure";
+        };
+      };
     };
   };
 }
