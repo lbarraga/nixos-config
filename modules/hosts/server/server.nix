8c8fa869f6014e381863970c9efaf37c6e99b616
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-04-01 23:59:12 +0200

    add docker to server

--- modules/hosts/server/server.nix
+++ modules/hosts/server/server.nix
@@ -1,5 +1,6 @@
-{den, ...}: {
-  den.hosts.x86_64-linux.server.users.lukasbt = {};
+{ den, ... }:
+{
+  den.hosts.x86_64-linux.server.users.lukasbt = { };
 
   den.aspects.server = {
     includes = with den.aspects; [
@@ -22,7 +23,6 @@
       zsh
 
       # services
-      immich-server
       docker
     ];
 
@@ -39,7 +39,7 @@
     };
 
     nixos = {
-      imports = [./_hardware-configuration.nix];
+      imports = [ ./_hardware-configuration.nix ];
       networking.hostName = "server";
     };
   };
