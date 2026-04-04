a57979281d05c965b2c7e04d94434856934137ca
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-04-04 20:10:51 +0200

    vps host

---
+++ modules/hosts/vps/vps.nix
@@ -0,0 +1,28 @@
+{den, ...}: {
+  den.hosts.x86_64-linux.vps.users.lukasbt = {};
+
+  den.aspects.vps = {
+    includes = with den.aspects; [
+      core
+      ssh-server
+      tailscale
+      cli-tools
+      git
+      nvim
+      zsh
+      nh
+      docker
+
+      # New services
+      pangolin
+      minecraft-server
+    ];
+
+    nixos = {
+      imports = [./_hardware-configuration.nix];
+      networking.hostName = "vps";
+
+      services.qemuGuest.enable = true;
+    };
+  };
+}
