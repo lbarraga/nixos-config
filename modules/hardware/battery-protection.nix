6884c7f3ada9c1ffbf919be2d1eab6ce1dbec74a
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-04-09 19:11:58 +0200

    target the right battery

--- modules/hardware/battery-protection.nix
+++ modules/hardware/battery-protection.nix
@@ -4,8 +4,8 @@
       services.tlp = {
         enable = true;
         settings = {
-          START_CHARGE_THRESH_BAT0 = 70; # Starts charging when it falls below this
-          STOP_CHARGE_THRESH_BAT0 = 80; # Stops charging when it reaches this
+          START_CHARGE_THRESH_BAT1 = 70; # Starts charging when it falls below this
+          STOP_CHARGE_THRESH_BAT1 = 80; # Stops charging when it reaches this
         };
       };
     };
